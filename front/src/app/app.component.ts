a08fffae474aa0c518013eea487589fc388d0813
Fuke Kazuki <fuke@jig.jp>
2022-06-01 14:55:38 +0900

    Initial Commit

---
+++ front/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import template from './app.component.html';
+
+@Component({
+  selector: 'app-root',
+  template,
+  //styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'esbuild';
+}
